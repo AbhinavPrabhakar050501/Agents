39e1ed7e738c036d1910330177197d0ee04eee79
Abhinav <abhinav2presidency@gmail.com>
2025-05-21 20:09:14 +0530

    Added pydantic basics

---
+++ Langgraph/1-pydantic/app.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel,Field
+from typing import Optional,List
+class Person(BaseModel):
+    name:str
+    age:int
+    city:str
+    where:Optional[float]
+
+person = Person(name="sa",age=24, city="f")
+
+class New(BaseModel):
+    name: str = Field(default="lol", description="")
